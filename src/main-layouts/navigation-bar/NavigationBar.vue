7eb2a5c1cda12628b73ce939087762724237d4a2
Ayush Poudel <ayushpoudel@usf.edu>
2025-06-15 13:29:02 -0400

    Initial plan and program design
    Closes #3

---
+++ src/main-layouts/navigation-bar/NavigationBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="nav-bar">
+    <div
+      v-for="section in navSections"
+      :key="section.base"
+      class="nav-section"
+      @mouseenter="hovered = section.base"
+      @mouseleave="hovered = null"
+    >
+      <RouterLink :to="section.base" class="nav-link">
+        {{ section.title }}
+      </RouterLink>
+
+      <div v-if="hovered === section.base" class="dropdown">
+        <RouterLink
+          v-for="sub in section.subpages"
+          :key="sub.path"
+          :to="sub.path"
+          class="dropdown-item"
+        >
+          {{ sub.title }}
+        </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useNavSections } from './useNavSections'
+
+const { navSections } = useNavSections()
+const hovered = ref(null)
+</script>
