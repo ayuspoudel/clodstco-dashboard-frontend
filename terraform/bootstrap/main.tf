9638f6812425b192baaf186464cf6d557a597d20
Ayush Poudel <ayushpoudel@usf.edu>
2025-07-27 19:01:00 -0400

    Modify github OIDC
    Closes #43

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "github_oidc_role" {
       Action = "sts:AssumeRoleWithWebIdentity",
       Condition = {
         StringEquals = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/${var.github_branch}"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}"
         }
       }
     }]
@@ -33,42 +33,4 @@ resource "aws_iam_role_policy_attachment" "attach_admin" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-resource "aws_s3_bucket" "tf_state" {
-  bucket        = var.bucket_name
-  force_destroy = true
-}
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.tf_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.tf_state.bucket
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_block" {
-  bucket = aws_s3_bucket.tf_state.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_dynamodb_table" "tf_lock" {
-  name           = var.dynamodb_table
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
