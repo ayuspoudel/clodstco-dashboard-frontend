fa99020a6d0aa03484078c30e0ed618f2844cac0
Ayush Poudel <ayushpoudel@usf.edu>
2025-08-03 17:54:44 -0400

    Fix ebs error

--- terraform/modules/aws-ec2/outputs.tf
+++ terraform/modules/aws-ec2/outputs.tf
@@ -11,8 +11,9 @@ output "public_ip" {
 }
 
 output "root_volume_id" {
-  value = aws_ebs_volume.root.id
+  value = aws_instance.this.root_block_device[0].volume_id
 }
+
 
 output "additional_volume_id" {
   value = try(aws_ebs_volume.additional[0].id, null)
