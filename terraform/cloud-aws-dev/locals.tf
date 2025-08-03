c8f5c2ef0badc24e633d6be7a5467d59dd297ebe
Ayush Poudel <ayushpoudel@usf.edu>
2025-08-03 17:17:04 -0400

    Add aws infra
    Closes #67

--- terraform/cloud-aws-dev/locals.tf
+++ terraform/cloud-aws-dev/locals.tf
@@ -5,7 +5,6 @@ locals {
     owner           = "ayuspoudel"
     team            = "platform"
     repo            = "ayuspoudel/clodstco-dashboard-frontend"
-    purpose         = "cloud-gateway" # can be rds / vpn / nat / general
     provisioned_by  = "Terraform"
     managed_by      = "IaC"
     created_by      = "github-actions"
